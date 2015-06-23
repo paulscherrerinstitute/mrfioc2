1964132d01a20e18c8b04f0f8930c8b2ec2c3344
Saso Skube <saso.skube@psi.ch>
2015-06-23 13:10:22 +0200

    all GUIs open script: changed SYS name

--- 300test/startGUI.sh
+++ 300test/startGUI.sh
@@ -1,15 +1,15 @@
 # SATS
-caqtdm -attach -macro "EVR=FTEST-MRFSATS-EVR0,FF=VME-300" G_EVR_master.ui &
+caqtdm -attach -macro "EVR=FTEST-VME-SATS-EVR0,FF=VME-300" G_EVR_master.ui &
 sleep 2
 # SATM
-caqtdm -attach -macro "EVG=FTEST-MRFSATM-EVG0" G_EVG_VME_master.ui &
+caqtdm -attach -macro "EVG=FTEST-VME-SATM-EVG0" G_EVG_VME_master.ui &
 sleep 2
-caqtdm -attach -macro "EVG=FTEST-MRFSATM-EVG1" G_EVG_VME_master.ui &
+caqtdm -attach -macro "EVG=FTEST-VME-SATM-EVG1" G_EVG_VME_master.ui &
 sleep 2 
-caqtdm -attach -macro "EVG=FTEST-MRFSATM-EVG2" G_EVG_VME_master.ui &
+caqtdm -attach -macro "EVG=FTEST-VME-SATM-EVG2" G_EVG_VME_master.ui &
 sleep 2
 
 #SATD
-caqtdm -attach -macro "EVG=FTEST-MRFSATD-EVG0" G_EVG_VME_master.ui &
+caqtdm -attach -macro "EVG=FTEST-VME-SATD-EVG0" G_EVG_VME_master.ui &
 sleep 2
-caqtdm -attach -macro "EVG=FTEST-MRFSATD-EVG1" G_EVG_VME_master.ui &
+caqtdm -attach -macro "EVG=FTEST-VME-SATD-EVG1" G_EVG_VME_master.ui &
