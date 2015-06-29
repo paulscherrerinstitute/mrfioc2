e695fab14a23195137028d7b30a5d772517b496a
Saso Skube <saso.skube@psi.ch>
2015-06-29 11:41:56 +0200

    prescaler read/write: using READ/WRITE macro

--- evrMrmApp/src/drvemPrescaler.cpp
+++ evrMrmApp/src/drvemPrescaler.cpp
@@ -24,16 +24,13 @@
 epicsUInt32
 MRMPreScaler::prescaler() const
 {
-    return nat_ioread32(base+U32_Scaler(id));  //FIXME must be READ32 instead of nat_ioread32
+    return READ32(base, Scaler(id));
 }
 
 void
 MRMPreScaler::setPrescaler(epicsUInt32 v)
 {
-    if(v>0xffff)
-        throw std::out_of_range("prescaler setting is out of range");
-
-    nat_iowrite32(base+U32_Scaler(id), v); // FIXME must be WRITE32 instead of nat_iowrite32
+    WRITE32(base, Scaler(id), v);
 }
 
 
