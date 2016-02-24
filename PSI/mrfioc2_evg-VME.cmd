0ac6c6e52e72068882de1b13973b719a266e2db7
Saso Skube <saso.skube@psi.ch>
2016-02-24 10:45:40 +0100

    EVG health: added health template to 300-series EVG substitutions and added startup script macro to select which SFP ports to monitor

--- PSI/mrfioc2_evg-VME.cmd
+++ PSI/mrfioc2_evg-VME.cmd
@@ -2,4 +2,4 @@
 var dbTemplateMaxVars 500
 
 mrmEvgSetupVME($(DEVICE=EVG0),$(EVG_SLOT=2),$(EVG_MEMOFFSET=0x000000000),$(EVG_IRQLINE=0x2),$(EVG_IRQVECT=0x1))
-dbLoadTemplate("$(EVG_SUBS=cfg/$(DEVICE=EVG0).subs)","SYS=$(SYS),DEVICE=$(DEVICE=EVG0)")
+dbLoadTemplate("$(EVG_SUBS=cfg/$(DEVICE=EVG0).subs)","SYS=$(SYS),DEVICE=$(DEVICE=EVG0),MON-PORTS=$(MON-PORTS=0x00)")
