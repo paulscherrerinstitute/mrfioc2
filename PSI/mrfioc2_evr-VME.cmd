f017df5439cb9bc9d9987d15a030d5e47a60167b
Saso Skube <saso.skube@psi.ch>
2016-04-01 11:49:16 +0200

    EVR: Default delay compensation target value set to 1000 and added record that can be used to set the same delay compensation target value throughout timing network

--- PSI/mrfioc2_evr-VME.cmd
+++ PSI/mrfioc2_evr-VME.cmd
@@ -3,7 +3,7 @@ var dbTemplateMaxVars 500
 
 mrmEvrSetupVME($(DEVICE=EVR0),$(EVR_SLOT=3),$(EVR_MEMOFFSET=0x3000000), $(EVR_IRQLINE=5), $(EVR_IRQVECT=0x26));
 
-dbLoadTemplate $(EVR_SUBS=cfg/$(DEVICE=EVR0).subs),"SYS=$(SYS),DEVICE=$(DEVICE=EVR0)"
+dbLoadTemplate $(EVR_SUBS=cfg/$(DEVICE=EVR0).subs),"SYS=$(SYS),DEVICE=$(DEVICE=EVR0), DC_SOURCE=$(DC_SOURCE=)"
 
 
 ##Health monitoring: 
