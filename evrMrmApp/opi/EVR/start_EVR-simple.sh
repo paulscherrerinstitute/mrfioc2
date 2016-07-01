0e7bddde4f52a6c5f447028818f16309cb717dda
Saso Skube <saso.skube@psi.ch>
2016-07-01 10:34:16 +0200

    Added simple panel to display pulser and output configurations

---
+++ evrMrmApp/opi/EVR/start_EVR-simple.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+set -o errexit
+
+SYS=""
+DEVICE="EVR0"
+FF="VME-300"
+ATTACH="-attach"
+
+usage()
+{
+    echo "Usage: $0 [options]"
+    echo "Options:"
+    echo "    -s <system name>     The system/project name"
+    echo "    -d <EVR name>        Event Receiver / timing card name (default: $EVR)"
+    echo "    -n                   Do not attach to existing caQtDM. Open new one instead"
+    echo "    -h                   This help"
+}
+
+while getopts ":s:d:nh" o; do
+    case "${o}" in
+        s)
+            SYS=${OPTARG}
+            ;;
+        d)
+            DEVICE=${OPTARG}
+            ;;
+        n)
+            ATTACH=""
+            ;;
+        h)
+            usage
+            exit 0
+            ;;
+        *)
+            usage
+            exit 1
+            ;;
+    esac
+done
+
+if [ $OPTIND -le 1 ]; then
+    usage
+    exit 1
+fi
+
+if [ -z $SYS ]; then
+    usage
+    exit 1
+fi
+
+macro="SYS=$SYS,DEVICE=$DEVICE"
+caqtdm $ATTACH -macro "$macro" G_EVR_SIMPLE.ui &
