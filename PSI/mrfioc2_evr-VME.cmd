588d3acc840f4f8033e708054b052d881ff4a55b
Tom Slejko <tom.slejko@cosylab.com>
2016-12-06 15:39:03 +0100

    Added beamok template

--- PSI/mrfioc2_evr-VME.cmd
+++ PSI/mrfioc2_evr-VME.cmd
@@ -5,6 +5,9 @@ mrmEvrSetupVME($(DEVICE=EVR0),$(EVR_SLOT=3),$(EVR_MEMOFFSET=0x3000000), $(EVR_IR
 
 dbLoadTemplate $(EVR_SUBS=cfg/$(DEVICE=EVR0).subs),"SYS=$(SYS),DEVICE=$(DEVICE=EVR0)"
 
+## PSI specific templates
+dbLoadRecords $(mrfioc2_TEMPLATES)/evr-BeamOK.template,"SYS=$(SYS),DEVICE=$(DEVICE=EVR0)"
+
 
 ## Health monitoring: 
 dbLoadRecords "$(mrfioc2_TEMPLATES=db)/evr-health.template", "SYS=$(SYS),DEVICE=$(DEVICE=EVR0)"
